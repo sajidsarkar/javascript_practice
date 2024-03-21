af6eb206ba68afb41649f77715b4465ef3c1d1b5
sajidsarkar <sajid.sarkar123@gmail.com>
2024-03-20 21:29:57 -0500

    merging into main

---
+++ index.js
@@ -0,0 +1,17 @@
+let selectedColor = ['red', 'blue'];
+selectedColor[2] = 4;
+// console.log(selectedColor.length);
+
+// //Function
+// function greet(names) {
+//     console.log('Name is '+names)
+// }
+
+// greet('bindu')
+
+// calculating a value with return
+function greet(n) {
+    return n*n;
+}
+
+console.log(greet(3));
