329977fb57a5661c7754794dbd3d30230ecff647
sajidsarkar <sajid.sarkar123@gmail.com>
2024-03-20 22:41:32 -0500

    made change to feature1 branch

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ let selectedColor = ['red', 'blue'];
 selectedColor[2] = 4;
 // console.log(selectedColor.length);
 
-// //Function
-// function greet(names) {
-//     console.log('Name is '+names)
-// }
-
-// greet('bindu')
-
-// calculating a value with return
-function greet(n) {
-    return n*n;
+//Function
+function greet(names) {
+    console.log('Name is '+names)
 }
 
-console.log(greet(3));
+greet('bindu')
+
+// // calculating a value with return
+// function greet(n) {
+//     return n*n;
+// }
+
+// console.log(greet(3));
